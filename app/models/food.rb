4779a7d0359d4af60f49f89f4e46fb73b01fe06e
しゅんや <bshunya0124@gmail.com>
2021-10-07 16:30:24 +0900

    エラーメッセージの日本語化

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
+  validates :name, presence: true
+  validates :name, {length: {maximum:11} }
+
   mount_uploader :image, ImageUploader
   
   enum management: {賞味期限:1, 消費期限:2, 管理しない:3}
