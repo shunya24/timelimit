812df6155233ee03c83397f99ae66e2ce3fa734f
しゅんや <bshunya0124@gmail.com>
2021-10-11 22:08:54 +0900

    修正

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,15 +3,15 @@ class Food < ApplicationRecord
   validates :name, {length: {maximum:11} }
 
   mount_uploader :image, ImageUploader
-  
+
   enum management: {賞味期限:1, 消費期限:2, 管理しない:3}
   def limit_day
-    now = "#{Time.zone.now.year}年#{Time.zone.now.month}/#{Time.zone.now.day}"
+    limit_day = "#{limit.year - 2000}年#{limit.month}/#{limit.day}"
 
-    if management == "管理しない"
-      "管理しない"
+    if management == '管理しない'
+      '管理しない'
     else
-      now
+      limit_day
     end
   end
 
@@ -21,18 +21,18 @@ class Food < ApplicationRecord
 
     "#{limit} g #{Time.zone.now}   aaa   #{days + 365}"
 
-    if management == "管理しない"
-      ""
+    if management == '管理しない'
+      ''
     elsif years == 0 && days < 0
-      "期限切れ"
+      '期限切れ'
     elsif years == 0 && days == 0
-      "今日まで"
+      '今日まで'
     elsif years == 0 && days > 0
       "あと#{days}日"
     elsif years < 0
-      "期限切れ"
+      '期限切れ'
     elsif years > 0 && days < 0
-      "あと#{years - 1}年と#{days + 365}日"
+      "あと#{years - 1}年#{days + 365}日"
     elsif years > 0 && days == 0
       "あと#{years}年"
     elsif years > 0 && days > 0
