831039fd4b29aebe218ef59be653657b733f4467
しゅんや <bshunya0124@gmail.com>
2021-10-16 23:37:02 +0900

    shoppingモデル作成

---
+++ app/models/sopping.rb
@@ -0,0 +1,2 @@
+class Sopping < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_063420) do
+ActiveRecord::Schema.define(version: 2021_10_16_142011) do
 
   create_table "foods", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_09_23_063420) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "soppings", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_soppings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
