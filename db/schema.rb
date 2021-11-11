4d6a46d73be832982f868ef1a775a2881fb98d46
しゅんや <bshunya0124@gmail.com>
2021-11-11 23:32:58 +0900

    profileモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_141312) do
+ActiveRecord::Schema.define(version: 2021_11_11_141712) do
 
   create_table "checks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2021_10_20_141312) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "nickname"
+    t.integer "my_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "shoppings", force: :cascade do |t|
