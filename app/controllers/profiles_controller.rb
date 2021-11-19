ede5673f1e18c7b005db3833e577097e4d65b6ea
しゅんや <bshunya0124@gmail.com>
2021-11-19 23:47:11 +0900

    profileを変更できるようにした

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,21 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    @profile = current_user.build_profile
   end
 
+  def update
+    @profile = current_user.build_profile
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:nickname, :my_id)
+  end
 end
