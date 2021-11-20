3fd99841a1521d79f2ae0a284fe59d5d5405812a
しゅんや <bshunya0124@gmail.com>
2021-11-20 23:46:07 +0900

    profileのlayout

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,7 +10,7 @@ class ProfilesController < ApplicationController
   end
 
   def update
-    @profile = current_user.build_profile
+    @profile = current_user.build_profile(profile_params)
     if @profile.save
       redirect_to profile_path, notice: 'プロフィール更新'
     else
