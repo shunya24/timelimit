f2f0ee45fb864c1d603097af96e7c448fd4c61aa
しゅんや <bshunya0124@gmail.com>
2021-11-26 23:55:16 +0900

    profile完成

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,10 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :nickname, presence: true
+  validates :nickname, length: {maximum: 13}
+
+  validates :my_id, presence: true
+  validates :my_id, {uniqueness: true}
+  validates :my_id, length: {maximum: 13}
 end
