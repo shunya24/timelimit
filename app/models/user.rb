59f1255a8edb7deff7fc9302845138c0ee72b0b0
しゅんや <bshunya0124@gmail.com>
2021-12-08 16:46:32 +0900

    followのテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,24 @@ class User < ApplicationRecord
   has_many :checks, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   def has_check?(shopping)
     checks.exists?(shopping_id: shopping.id)
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
   end
 
   def avatar_image
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_141712) do
+ActiveRecord::Schema.define(version: 2021_12_06_083231) do
 
   create_table "checks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2021_11_11_141712) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
+  end
+
   create_table "shoppings", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name", null: false
@@ -64,4 +73,6 @@ ActiveRecord::Schema.define(version: 2021_11_11_141712) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "relationships", "users", column: "follower_id"
+  add_foreign_key "relationships", "users", column: "following_id"
 end
