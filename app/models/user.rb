53baf12c076580bd19d90fdfdb0c5766868e44ef
しゅんや <bshunya0124@gmail.com>
2022-01-02 23:55:22 +0900

    ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,4 +59,12 @@ class User < ApplicationRecord
       'user.png'
     end
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
 end
