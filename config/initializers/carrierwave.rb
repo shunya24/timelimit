8198d72ff73278942d8a8acae046c3d647227ea4
しゅんや <bshunya0124@gmail.com>
2022-01-09 17:17:09 +0900

    画像投稿ができないバグの修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'time-limit'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/time-limit'
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY'],
